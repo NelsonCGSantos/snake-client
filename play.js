f75f079e085dcf3678ac1dc0ff6d69c958d36ebd
NelsonCGSantos <nelsoncgsantos@gmail.com>
2024-07-03 16:21:37 -0600

    3

--- play.js
+++ play.js
@@ -2,6 +2,25 @@ const { connect } = require("./client");
 
 const net = require("net");
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+  
+const handleUserInput = function (key) {
+  if (key === "\u0003") {
+    // ctrl + c
+    process.exit();
+  }
+};
+
+setupInput();
+  
+
 // conn.write("Move: up");
 
 // For setInterval example
