a65fb342c34b5040dd550491d079af9830cd5095
NelsonCGSantos <nelsoncgsantos@gmail.com>
2024-07-03 16:15:46 -0600

    2

--- client.js
+++ client.js
@@ -14,6 +14,7 @@ const connect = function () {
 
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
+    // conn.write("Move: up");
   });
 
   return conn;
--- play.js
+++ play.js
@@ -2,7 +2,10 @@ const { connect } = require("./client");
 
 const net = require("net");
 
+// conn.write("Move: up");
 
+// For setInterval example
+// clearInterval(intervalId);
 
 
 console.log("Connecting ...");
